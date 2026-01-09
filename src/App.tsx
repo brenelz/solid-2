65c0d5d3a2422e0272e5c4f2448324da287c88b6
Brenley Dueck <brenleydueck@gmail.com>
2026-01-08 21:29:24 -0600

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
-import { createSignal } from 'solid-js'
+import { createEffect, createMemo, createSignal, Errored, isPending, isRefreshing, Loading, refresh, Show } from "solid-js";
 
-function App() {
-  const [count, setCount] = createSignal(0)
-
-  return (
-    <>
-      {count()}
-      <button onClick={() => setCount(count() + 1)}>Increment</button>
-    </>
-  )
+const users = {
+  0: 'John Doe',
+  1: 'Jane Doe',
+  2: 'Jim Doe',
 }
 
-export default App
+async function fetchUser(userId: number) {
+  console.log('fetching user', userId);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const user = users[userId as keyof typeof users];
+  if (!user) {
+    throw new Error('User not found');
+  }
+  return user;
+}
+
+export default function App() {
+  const [id, setId] = createSignal(0);
+  const [user, setUser] = createSignal(() => fetchUser(id()));
+  const upper = createMemo(() => user().toUpperCase());
+
+  return (
+
+    <Loading fallback={<div>Loading...</div>}>
+      <Errored fallback={<div>Error</div>}>
+        <div style={{ opacity: isPending(upper) ? 0.5 : 1 }}>{upper()}</div>
+      </Errored>
+      <p>
+        <button onClick={() => setId(id() - 1)}>Previous</button>
+        <button onClick={() => setId(id() + 1)}>Next</button>
+        <button onClick={() => refresh(user)}>Refresh</button>
+        <button onClick={() => setUser('My User')}>Set User</button>
+      </p>
+    </Loading >
+  )
+}
